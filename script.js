05c6adcf008a9be5bd2bd5b08ed5c683d7b0d35b
Alessandra Lemos <allalessandra582@gmail.com>
2025-03-04 20:38:51 -0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// verificação automática quando carregar a página
-window.onload = function () {
-  const html = document.documentElement // pega o html
-  const img = document.querySelector("#profile img") // pega a img do meu perfil
-
-  // verifica se já está no modo claro
-  if (html.classList.contains('light')) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Alessandra usando óculos, fundo amadeirado com parede branca."
-    )
-  }
-}
-
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
